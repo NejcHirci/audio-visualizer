e2691e682dbbe32a678411abf29404df57680437
NejcHirci <nhirci@gmail.com>
2022-11-26 10:52:25 +0100

    New things

--- src/client/webpack.common.js
+++ src/client/webpack.common.js
@@ -9,6 +9,10 @@ module.exports = {
                 use: 'ts-loader',
                 exclude: /node_modules/,
             },
+            {
+                test: /\.mp3/,
+                use: 'file-loader'
+            }
         ],
     },
     resolve: {
